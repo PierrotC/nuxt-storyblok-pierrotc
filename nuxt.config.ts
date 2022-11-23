8e6428444d4892c8e3c189880666e0b746944945
PierrotC <pierre-courteille@outlook.fr>
2022-11-23 19:12:39 +0100

    End of workshop, exercise 4 in progress

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    modules: [
+        [
+            '@storyblok/nuxt',
+            {
+                accessToken: 'JquP8cdjg0QYiYLbu3wi8gtt',
+            }
+        ],
+        '@nuxtjs/tailwindcss'
+      ],
 })
